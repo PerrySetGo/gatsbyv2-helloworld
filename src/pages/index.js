de421ec49fd374d87c5ab623e636f05d99b8f279
Perry Eising <perry.r.eising@gmail.com>
2018-09-17 16:24:41 -0700

    minor edits, link, props

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default () => (
+  <section>
+  <Header />
   <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
+  <h1>some test stuff!</h1>
+  <Link to="/contact/">Contact</Link>
+
+</section>
 )
